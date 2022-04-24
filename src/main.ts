5bcbc808a1940e00da861d75ed4bb1170b12af05
Antoine Chalifour <antoine.chalifour@gmail.com>
2022-04-24 16:02:06 +0200

    chore: stuff

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from "@nestjs/core";
 import { Module } from "@nestjs/common";
-import express from "express";
 import { RemixModule } from "remix-bridge";
 
 @Module({
@@ -10,19 +9,8 @@ class AppModule {}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bodyParser: false });
-  const server = app.getHttpAdapter() as any;
 
-  server
-    .use(
-      "/build",
-      express.static("public/build", { immutable: true, maxAge: "1y" })
-    )
-    .use(
-      "/fonts",
-      express.static("public/fonts", { immutable: true, maxAge: "1y" })
-    );
-
-  await app.listen(3000);
+  return app.listen(process.env.PORT ?? 3000);
 }
 
 bootstrap();
