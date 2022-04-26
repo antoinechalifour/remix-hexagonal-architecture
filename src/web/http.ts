a9582e082148b8284a4cc8d5d7b0eceb87494874
Antoine Chalifour <antoine.chalifour@gmail.com>
2022-04-26 12:52:16 +0200

    fix: reflection dto class

--- src/web/http.ts
+++ src/web/http.ts
@@ -12,23 +12,6 @@ const formatErrors = (error: Joi.ValidationError) => {
   return errors;
 };
 
-type ValidationResult<T> = [Record<string, string>, null] | [null, T];
-
-export const validateBody = async <T = unknown>(
-  schema: Joi.ObjectSchema<T>,
-  request: Request
-): Promise<ValidationResult<T>> => {
-  const { value, error } = schema.validate(
-    Object.fromEntries(await request.formData())
-  );
-
-  if (error?.details) {
-    return [formatErrors(error), null];
-  }
-
-  return [null, value!];
-};
-
 export const requireAuthentication = async (request: Request) => {
   const session = await sessionFromCookies(request);
 
