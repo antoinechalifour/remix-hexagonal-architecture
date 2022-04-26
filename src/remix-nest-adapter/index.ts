e3ad02c0af5a1dff3a082c5251027beb93a027e5
Antoine Chalifour <antoine.chalifour@gmail.com>
2022-04-26 12:28:49 +0200

    dynamic types for context

--- src/remix-nest-adapter/index.ts
+++ src/remix-nest-adapter/index.ts
@@ -1,3 +1,14 @@
+import { ActionFunction, LoaderFunction } from "remix";
+
 export * from "./RemixModule";
 export * from "./RemixNestBuildConfig";
 export * from "./keys";
+
+export type MakeRemixContext<Actions, Loaders> = {
+  actions: {
+    [key in keyof Actions]: ActionFunction;
+  };
+  loaders: {
+    [key in keyof Loaders]: LoaderFunction;
+  };
+};
