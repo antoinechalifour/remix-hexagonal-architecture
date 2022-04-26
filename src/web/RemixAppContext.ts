85782c28a964c631314bf8b61179dde011b89226
Antoine Chalifour <antoine.chalifour@gmail.com>
2022-04-26 12:28:49 +0200

    dynamic types for context

--- src/web/RemixAppContext.ts
+++ src/web/RemixAppContext.ts
@@ -1,12 +1,5 @@
-import { ActionFunction, LoaderFunction } from "remix";
+import { MakeRemixContext } from "remix-nest-adapter";
 import { Actions } from "./Actions";
 import { Loaders } from "./Loaders";
 
-export type RemixAppContext = {
-  actions: {
-    [key in keyof Actions]: ActionFunction;
-  };
-  loaders: {
-    [key in keyof Loaders]: LoaderFunction;
-  };
-};
+export type RemixAppContext = MakeRemixContext<Actions, Loaders>;
