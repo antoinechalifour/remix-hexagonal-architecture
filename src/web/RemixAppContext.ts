012c54434b3e870389fbe308477fdb0202f687b9
Antoine Chalifour <antoine.chalifour@gmail.com>
2022-04-26 12:27:18 +0200

    front type safety

--- src/web/RemixAppContext.ts
+++ src/web/RemixAppContext.ts
@@ -1,7 +1,12 @@
+import { ActionFunction, LoaderFunction } from "remix";
 import { Actions } from "./Actions";
 import { Loaders } from "./Loaders";
 
 export type RemixAppContext = {
-  actions: Actions;
-  loaders: Loaders;
+  actions: {
+    [key in keyof Actions]: ActionFunction;
+  };
+  loaders: {
+    [key in keyof Loaders]: LoaderFunction;
+  };
 };
