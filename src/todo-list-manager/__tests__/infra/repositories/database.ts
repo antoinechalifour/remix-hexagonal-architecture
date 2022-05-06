0be67cf9a67f2e6d029363e594d28cf81eaa838e
Antoine Chalifour <antoine.chalifour@gmail.com>
2022-05-06 20:43:40 +0200

    chore: migrate to postgresql

--- src/todo-list-manager/__tests__/infra/repositories/database.ts
+++ src/todo-list-manager/__tests__/infra/repositories/database.ts
@@ -2,9 +2,9 @@ import { execSync } from "child_process";
 
 export async function prepareDatabase() {
   execSync("yarn integration:clean");
-  execSync("yarn integration:prepare");
 }
 
 export function configureTestingDatabaseEnvironment() {
-  process.env.DATABASE_URL = "file:./integration_todolists.db";
+  process.env.DATABASE_URL =
+    "postgresql://user:password@localhost:6070/todolistmanager-integration";
 }
--- src/todo-list-manager/query/FetchTodoListPrismaQuery.ts
+++ src/todo-list-manager/query/FetchTodoListPrismaQuery.ts
@@ -28,23 +28,23 @@ export class FetchTodoListPrismaQuery implements FetchTodoListQuery {
 
   private fetchTodoList(todoListId: TodoListId) {
     return this.prisma.$queryRaw<any[]>`
-        SELECT TL.id, TL.title, TL.createdAt
-        FROM TodoList TL
+        SELECT TL.id, TL.title, TL."createdAt"
+        FROM "TodoList" TL
         WHERE TL.id=${todoListId};
     `.then((rows) => rows[0]);
   }
 
   private fetchDoingTodos(todoListId: TodoListId) {
     return this.prisma.$queryRaw<any[]>`
-        SELECT id, title, isComplete, createdAt FROM Todo
-        WHERE isComplete IS false AND todoListId=${todoListId};
+        SELECT id, title, "isComplete", "createdAt" FROM "Todo"
+        WHERE "isComplete" IS false AND "todoListId"=${todoListId};
     `;
   }
 
   private fetchCompleteTodos(todoListId: TodoListId) {
     return this.prisma.$queryRaw<any[]>`
-        SELECT id, title, isComplete, createdAt FROM Todo
-        WHERE isComplete IS true AND todoListId=${todoListId};
+        SELECT id, title, "isComplete", "createdAt" FROM "Todo"
+        WHERE "isComplete" IS true AND "todoListId"=${todoListId};
     `;
   }
 }
