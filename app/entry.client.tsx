1efb5357ed85401feccf6a207272a686af2853b0
Antoine Chalifour <antoine.chalifour@gmail.com>
2022-05-08 18:36:26 +0200

    chore: use startTransition for todos ordering while dragging

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,4 +1,3 @@
-import { hydrate } from "react-dom";
-import { RemixBrowser } from "remix";
-
-hydrate(<RemixBrowser />, document);
+import { RemixBrowser } from "@remix-run/react";
+import { hydrateRoot } from "react-dom/client";
+hydrateRoot(document, <RemixBrowser />);
