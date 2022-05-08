19070a80dee9ef5c4eb712ea7f2abfa3c4274715
Antoine Chalifour <antoine.chalifour@gmail.com>
2022-05-08 18:27:04 +0200

    chore: upgrade packages to React 18

--- app/components/ErrorPage.tsx
+++ app/components/ErrorPage.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 
-export const ErrorPage: React.FC = ({ children }) => (
+export type ErrorComponentProps = { children: React.ReactNode };
+
+export const ErrorPage = ({ children }: ErrorComponentProps) => (
   <div className="my-24 grid gap-6 text-center">{children}</div>
 );
 
-export const ErrorPageHero: React.FC = ({ children }) => (
+export const ErrorPageHero = ({ children }: ErrorComponentProps) => (
   <h1 className="text-9xl">{children}</h1>
 );
 
-export const ErrorPageMessage: React.FC = ({ children }) => <p>{children}</p>;
+export const ErrorPageMessage = ({ children }: ErrorComponentProps) => (
+  <p>{children}</p>
+);
