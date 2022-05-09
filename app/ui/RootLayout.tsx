55f73e70fb73757f5e57c5d2de3e264d23b5a67f
Antoine Chalifour <antoine.chalifour@gmail.com>
2022-05-09 22:44:14 +0200

    feat: sticky header

--- app/ui/RootLayout.tsx
+++ app/ui/RootLayout.tsx
@@ -3,7 +3,7 @@ import { Link } from "remix";
 
 export const RootLayout = ({ children }: React.PropsWithChildren<{}>) => (
   <div className="grid min-h-screen grid-rows-[auto_1fr] gap-4 bg-darker">
-    <header className="bg-dark py-6 font-semibold text-lighter shadow-lg">
+    <header className="sticky top-0 bg-dark py-6 font-semibold text-lighter shadow-lg">
       <div className="mx-auto max-w-5xl px-6">
         <Link to="/">Todo List Manager</Link>
       </div>
