b5f43b92b04abb660e2d51cca2579bce5807dc20
Antoine Chalifour <antoine.chalifour@gmail.com>
2022-05-17 20:56:40 +0200

    feat: tag todo usecase

--- src/todo-list-manager/domain/Todo.ts
+++ src/todo-list-manager/domain/Todo.ts
@@ -10,6 +10,7 @@ export type Todo = {
   createdAt: string;
   todoListId: TodoListId;
   ownerId: OwnerId;
+  tags: string[];
 };
 
 export const updateCompletion = (todo: Todo, isComplete: boolean) => ({
@@ -21,3 +22,12 @@ export const renameTodo = (todo: Todo, title: string) => ({
   ...todo,
   title,
 });
+
+export const tagTodo = (todo: Todo, tag: string) => {
+  if (todo.tags.includes(tag)) return todo;
+
+  return {
+    ...todo,
+    tags: [...todo.tags, tag],
+  };
+};
