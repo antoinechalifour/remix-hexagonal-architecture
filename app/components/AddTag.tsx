10619b071508881978cfdf4e6a95376e5b5ab05e
Antoine Chalifour <antoine.chalifour@gmail.com>
2022-05-19 13:59:11 +0200

    feat: reset input on submit

--- app/components/AddTag.tsx
+++ app/components/AddTag.tsx
@@ -1,5 +1,5 @@
 import type { TodoDto, TodoListDto } from "shared";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useFetcher } from "@remix-run/react";
 import { Popover } from "front/ui/Popover";
 
@@ -10,6 +10,16 @@ export type AddTagProps = {
 
 export const AddTag = ({ todoList, todo }: AddTagProps) => {
   const tagTodo = useFetcher();
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (tagTodo.type !== "done") return;
+
+    const input = inputRef.current;
+    if (input == null) return;
+    input.value = "";
+  }, [tagTodo.type]);
+
   return (
     <Popover.Item>
       <tagTodo.Form
@@ -17,9 +27,12 @@ export const AddTag = ({ todoList, todo }: AddTagProps) => {
         action={`/l/${todoList.id}/todo/${todo.id}/tag`}
       >
         <input
+          ref={inputRef}
           className="ml-4 w-[15ch] rounded bg-gray-200 py-1 px-2 font-mono text-xs text-gray-700"
           placeholder="New tag..."
           name="tag"
+          required
+          maxLength={15}
         />
 
         <button type="submit" className="sr-only">
