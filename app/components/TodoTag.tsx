7a89280b7935f4200bdf6c5b4bede528fab6867b
Antoine Chalifour <antoine.chalifour@gmail.com>
2022-05-19 08:53:45 +0200

    feat: tags in todo design

--- app/components/TodoTag.tsx
+++ app/components/TodoTag.tsx
@@ -6,7 +6,7 @@ export type TodoTagProps = { children: ReactNode; className?: string };
 export const TodoTag = ({ children, className }: TodoTagProps) => (
   <span
     className={classNames(
-      "rounded bg-sky-100 py-1 px-2 font-mono text-xs text-sky-700",
+      "truncate rounded bg-sky-100 py-1 px-2 font-mono text-xs text-sky-700",
       className
     )}
   >
