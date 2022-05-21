081633f5e8b6f3687f62b5cc37d661be4531cdeb
Antoine Chalifour <antoine.chalifour@gmail.com>
2022-05-21 20:47:13 +0200

    feat: forgot password page

--- app/ui/PageTitle.tsx
+++ app/ui/PageTitle.tsx
@@ -1,5 +1,13 @@
 import React from "react";
+import classNames from "classnames";
 
-export const PageTitle: React.FC = ({ children }) => (
-  <h1 className="text-2xl font-semibold text-lighter">{children}</h1>
+type PageTitleProps = {
+  children: React.ReactNode;
+  className?: string;
+};
+
+export const PageTitle = ({ children, className }: PageTitleProps) => (
+  <h1 className={classNames("text-2xl font-semibold text-lighter", className)}>
+    {children}
+  </h1>
 );
