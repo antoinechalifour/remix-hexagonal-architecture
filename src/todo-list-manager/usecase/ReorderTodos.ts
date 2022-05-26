a3495a3faa4a167925dff192706650545f4303cc
Antoine Chalifour <antoine.chalifour@gmail.com>
2022-05-26 21:28:13 +0200

    feat: check permissions for todos reordering

--- src/todo-list-manager/usecase/ReorderTodos.ts
+++ src/todo-list-manager/usecase/ReorderTodos.ts
@@ -1,20 +1,27 @@
 import type { TodoLists } from "../domain/TodoLists";
 import type { TodoListId } from "../domain/TodoList";
-import type { OwnerId } from "../domain/OwnerId";
 import type { TodoId } from "../domain/Todo";
+import type { TodoListPermissions } from "../domain/TodoListPermissions";
+import type { CollaboratorId } from "../domain/CollaboratorId";
 import { reorderTodoList } from "../domain/TodoList";
+import { canEditTodoList } from "../domain/TodoListPermission";
 
 export class ReorderTodos {
-  constructor(private readonly todoLists: TodoLists) {}
+  constructor(
+    private readonly todoLists: TodoLists,
+    private readonly todoListPermissions: TodoListPermissions
+  ) {}
 
   async execute(
     todoListId: TodoListId,
-    ownerId: OwnerId,
+    collaboratorId: CollaboratorId,
     todoToReorderId: TodoId,
     newIndex: number
   ) {
-    const todoList = await this.todoLists.ofId(todoListId, ownerId);
+    const permission = await this.todoListPermissions.ofTodoList(todoListId);
+    canEditTodoList(permission, collaboratorId);
 
+    const todoList = await this.todoLists.ofId(todoListId, collaboratorId);
     await this.todoLists.save(
       reorderTodoList(todoList, todoToReorderId, newIndex)
     );
