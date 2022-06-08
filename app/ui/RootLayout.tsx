2244746d9273654906fa8b0ac1f6e09634b5b06f
Antoine Chalifour <antoine.chalifour@gmail.com>
2022-06-08 23:23:11 +0200

    feat: skip to content

--- app/ui/RootLayout.tsx
+++ app/ui/RootLayout.tsx
@@ -1,27 +1,31 @@
-import * as React from "react";
+import React from "react";
 import { Link } from "@remix-run/react";
+import { SkipToContent } from "front/ui/SkipToContent";
 
 export const RootLayout = ({
   children,
   authenticated,
 }: React.PropsWithChildren<{ authenticated: boolean }>) => (
-  <div className="grid min-h-screen grid-rows-[auto_1fr] gap-4 bg-darker">
-    <header className="sticky top-0 z-10 bg-dark py-2 font-semibold text-lighter shadow-lg">
-      <div className="mx-auto flex max-w-5xl items-center justify-between px-3">
-        <Link to="/">
-          <img
-            src="/assets/images/todos-white.svg"
-            alt="Todos"
-            className="w-[75px]"
-          />
-        </Link>
+  <>
+    <SkipToContent />
+    <div className="grid min-h-screen grid-rows-[auto_1fr] gap-4 bg-darker">
+      <header className="sticky top-0 z-10 bg-dark py-2 font-semibold text-lighter shadow-lg">
+        <div className="mx-auto flex max-w-5xl items-center justify-between px-3">
+          <Link to="/">
+            <img
+              src="/assets/images/todos-white.svg"
+              alt="Todos"
+              className="w-[75px]"
+            />
+          </Link>
 
-        {authenticated && <Link to="/logout">Logout</Link>}
-      </div>
-    </header>
+          {authenticated && <Link to="/logout">Logout</Link>}
+        </div>
+      </header>
 
-    <main className="mx-auto w-full max-w-5xl py-2 px-3  pb-20">
-      {children}
-    </main>
-  </div>
+      <main id="main" className="mx-auto w-full max-w-5xl py-2 px-3  pb-20">
+        {children}
+      </main>
+    </div>
+  </>
 );
---
+++ app/ui/SkipToContent.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const SkipToContent = () => (
+  <a
+    href="#main"
+    className="fixed top-2 left-1/2 z-50 -translate-x-1/2 bg-white py-2 px-4 text-xs text-inverse underline opacity-0 transition-opacity focus:opacity-100"
+  >
+    Skip to content
+  </a>
+);
